2850a4ecc3fecea86babf6a2b718a93d4a7a33fd
yngtodd <young.todd.mk@gmail.com>
2020-10-10 16:53:57 -0400

    Add tensor device placement

---
+++ std/tensor.py
@@ -0,0 +1,26 @@
+import torch
+
+
+def to_device(tensor, device: torch.device):
+    r"""Convert tensor-like object to given PyTorch device
+
+    Args:
+        tensor: tensor or container of tensor-like objects
+        device: device to place the tensor upon
+
+    Returns:
+        the tensor place on the device
+    """
+    if tensor is None:
+        return tensor
+    elif isinstance(tensor, torch.Tensor):
+        return tensor.to(device)
+    elif isinstance(tensor, dict):
+        return {k: to_device(v, device) for k, v in tensor.items()}
+    elif isinstance(tensor, list):
+        return [to_device(v, device) for v in tensor]
+    elif isinstance(tensor, tuple):
+        return tuple(to_device(v, device) for v in tensor)
+    else:
+        raise NotImplementedError
+
